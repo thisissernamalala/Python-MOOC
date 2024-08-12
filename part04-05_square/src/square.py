89496704cd4c7f6a91e91c28f75957970361f9e9
thisissernamalala <222650494@mycput.ac.za>
2024-08-12 20:01:29 +0200

    Ex square hash with unique characters

--- part04-05_square/src/square.py
+++ part04-05_square/src/square.py
@@ -1,8 +1,17 @@
 # Copy here code of line function from previous exercise
+def line(i, s):
+    if s == "":
+        print(int(i)*"*")
+    elif len(s)>1:
+        print(int(i)*s[0])
+    else:
+        print(int(i)*s)
 
 def square(size, character):
-    # You should call function line here with proper parameters
-    line(4, "x")
+    i = 0
+    while i<size:
+        line(size, character)
+        i+=1
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
