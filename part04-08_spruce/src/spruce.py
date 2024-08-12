e04e572a268a47ce413980a4dbc7abbc149e4dda
thisissernamalala <222650494@mycput.ac.za>
2024-08-12 21:01:17 +0200

    spruce, chrismas tree code - found it difficult

--- part04-08_spruce/src/spruce.py
+++ part04-08_spruce/src/spruce.py
@@ -1,4 +1,28 @@
 # Write your solution here
+# def spruce(num):
+#     i = 1
+#     jump = 1
+#     ii = 0
+#     while i<=num:
+        
+#         print((ii+jump)*"*")
+#         jump+=1
+#         ii+=1
+#         i+=1
+
+def spruce(num):
+    print("a spruce!")
+    j = 0
+    k = 1
+    sp = num -1
+    lssp = num -1 
+    while j<num:
+        i = (2*k) - 1
+        print(sp*" "+i*"*"+sp*" ")
+        j+=1
+        k+=1
+        sp-=1
+    print(lssp*" "+"*"+lssp*" " )
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     spruce(5)
