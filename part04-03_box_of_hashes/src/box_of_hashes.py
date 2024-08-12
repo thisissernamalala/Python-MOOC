b80cc6a2c41987b49e2bd59a59e8cf312d40d735
thisissernamalala <222650494@mycput.ac.za>
2024-08-12 19:54:47 +0200

    Ex 3 box of hashes

--- part04-03_box_of_hashes/src/box_of_hashes.py
+++ part04-03_box_of_hashes/src/box_of_hashes.py
@@ -1,8 +1,19 @@
 # Copy here code of line function from previous exercise
+def line(i, s):
+    if s == "":
+        print(int(i)*"*")
+    elif len(s)>1:
+        print(int(i)*s[0])
+    else:
+        print(int(i)*s)
 
 def box_of_hashes(height):
+    i = 0
+    while i<height:
+        line(10, "#")
+        i+=1
     # You should call function line here with proper parameters
-    line(10, "#")
+    
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
