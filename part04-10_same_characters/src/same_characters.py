80fc83158cceb411a03ebad895dc621a17bd50e4
thisissernamalala <222650494@mycput.ac.za>
2024-08-12 21:13:42 +0200

    same characters & true and lase using len() function

--- part04-10_same_characters/src/same_characters.py
+++ part04-10_same_characters/src/same_characters.py
@@ -1,4 +1,12 @@
 # Write your solution here
+def same_chars(s,v1,v2):
+    maxx = len(s) - 1
+    if v1 > maxx or v2 > maxx:
+        return False
+    elif s[v1] == s[v2]:
+        return True
+    else:
+        return False
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     print(same_chars("coder", 1, 2))
