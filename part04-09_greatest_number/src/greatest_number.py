beb96c8c61b193ff79f3a082074c85fd5294d646
thisissernamalala <222650494@mycput.ac.za>
2024-08-12 21:06:22 +0200

    greatest number function

--- part04-09_greatest_number/src/greatest_number.py
+++ part04-09_greatest_number/src/greatest_number.py
@@ -1,4 +1,13 @@
 # Write your solution here
+def greatest_number(a,b,c):
+    res = 0
+    if a>=b and a>=c:
+        res = a
+    elif b>=a and b>=c:
+        res = b
+    elif c>=a and c>=b:
+        res = c
+    return res
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     greatest = greatest_number(5, 4, 8)
