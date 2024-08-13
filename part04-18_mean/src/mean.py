1b5917cc4277156e8bba132702bf63e51b120c92
thisissernamalala <222650494@mycput.ac.za>
2024-08-13 20:40:32 +0200

    get the mean using lists

--- part04-18_mean/src/mean.py
+++ part04-18_mean/src/mean.py
@@ -1,4 +1,9 @@
 # Write your solution here
+def mean(list):
+    length = len(list)
+    summ = sum(list)
+    av = summ/length
+    return av
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
