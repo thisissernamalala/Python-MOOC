0d103c9e0f09a5e1a9f4e001ec359ff0fff0ed3c
thisissernamalala <222650494@mycput.ac.za>
2024-08-13 20:00:54 +0200

    lists and indexes

--- part04-12_change_value_of_item/src/change_value_of_item.py
+++ part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,1 +1,11 @@
 # Write your solution here
+cur_list = [1, 2, 3, 4, 5]
+while True:
+    ind = int(input("Index: "))
+    if ind == -1:
+        break
+    else:
+        new_val= int(input("New value: "))
+        cur_list[ind] = new_val
+        print(cur_list)
+    
