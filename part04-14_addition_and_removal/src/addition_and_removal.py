b98678d26bca11879b5b81e7ced5d0d73606fe2b
thisissernamalala <222650494@mycput.ac.za>
2024-08-13 20:19:26 +0200

    adding and removing items in lists using list.append() and list.pop(-1)

--- part04-14_addition_and_removal/src/addition_and_removal.py
+++ part04-14_addition_and_removal/src/addition_and_removal.py
@@ -1,1 +1,21 @@
 # Write your solution here
+ind = 1
+list = []
+while True:
+    print(f"The list is now {list}")
+    inn = input("a(d)d, (r)emove or e(x)it: ")
+    if inn == "x":
+        print("Bye!")
+        break
+    elif inn == "d":
+        list.append(ind)
+        ind+=1
+        continue
+    elif inn == "r":
+        list.pop(-1)
+        ind-=1
+        continue
+        
+
+
+
