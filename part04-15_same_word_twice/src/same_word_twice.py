df55c0e4d11d858b15f46b6be6d311cac0c63003
thisissernamalala <222650494@mycput.ac.za>
2024-08-13 20:23:23 +0200

    Same word twice - checking by using a list

--- part04-15_same_word_twice/src/same_word_twice.py
+++ part04-15_same_word_twice/src/same_word_twice.py
@@ -1,1 +1,15 @@
 # Write your solution here
+list = []
+num = 0
+while True:
+    word = input("Word: ")
+    
+    if word in list:
+        print(f"You typed in {num} different words")
+        break
+
+    else:
+        list.append(word)
+        num+=1
+        continue
+
