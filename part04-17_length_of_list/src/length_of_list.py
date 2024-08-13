85398cc9caccc49ab33994c0f732f6a05d8d795f
thisissernamalala <222650494@mycput.ac.za>
2024-08-13 20:37:45 +0200

    get length of list

--- part04-17_length_of_list/src/length_of_list.py
+++ part04-17_length_of_list/src/length_of_list.py
@@ -1,4 +1,6 @@
 # Write your solution here
+def length(list):
+    return len(list)
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
