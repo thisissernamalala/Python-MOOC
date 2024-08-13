b62db71098396abb0fb65fc496dc8c659bc2b3bb
thisissernamalala <222650494@mycput.ac.za>
2024-08-13 20:07:28 +0200

    adding stuff to lists using list.append()

--- part04-13_add_items_to_list/src/add_items_to_list.py
+++ part04-13_add_items_to_list/src/add_items_to_list.py
@@ -1,1 +1,9 @@
 # Write your solution here
+
+size= int(input("How many items: "))
+list = []
+for i in range(1,size+1):
+    item = int(input(f"Item {i}: "))
+    list.append(item)
+print(list)
+    
