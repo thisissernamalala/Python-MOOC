8914b691b8906d9051f1e5a1371f9e2c3d43799d
thisissernamalala <222650494@mycput.ac.za>
2024-08-13 20:29:50 +0200

    Ordering lists - using list.append() and sorted(list)

--- part04-16_list_twice/src/list_twice.py
+++ part04-16_list_twice/src/list_twice.py
@@ -1,1 +1,16 @@
 # Write your solution here
+og  = []
+
+
+while True:
+    inn = int(input("New item: "))
+
+    if inn == 0:
+        print("Bye!")
+        break
+    else:
+        og.append(inn)
+        print(f"The list now: {og}")
+        changed = sorted(og)
+        print(f"The list in order: {changed}")
+        continue
