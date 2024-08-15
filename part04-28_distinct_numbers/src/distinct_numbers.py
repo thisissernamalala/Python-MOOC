543b8d85a16641dba7f2dd4d181a3f0a2445cb1a
thisissernamalala <222650494@mycput.ac.za>
2024-08-15 22:15:48 +0200

    distinct numbers exercise

--- part04-28_distinct_numbers/src/distinct_numbers.py
+++ part04-28_distinct_numbers/src/distinct_numbers.py
@@ -1,1 +1,9 @@
 # Write your solution here
+
+def distinct_numbers(list):
+    new_list = []
+    for i in list:
+        if i not in new_list:
+            new_list.append(i)
+    final_list = sorted(new_list)
+    return final_list
