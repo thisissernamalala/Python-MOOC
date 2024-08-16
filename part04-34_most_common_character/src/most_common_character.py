af92012b09305eed31af94a225e5254fc7ea762e
thisissernamalala <222650494@mycput.ac.za>
2024-08-16 21:54:27 +0200

    most common character

--- part04-34_most_common_character/src/most_common_character.py
+++ part04-34_most_common_character/src/most_common_character.py
@@ -1,1 +1,13 @@
 # Write your solution here
+
+def most_common_character(string):
+    chars = []
+    occs = 0
+    for i in range(len(string)):
+        chars.append(string[i])
+    for j in chars:
+        cnt = chars.count(j)
+        if cnt>occs:
+            occs = cnt
+            rs = j
+    return rs
