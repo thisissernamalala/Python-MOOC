4ce84dc728eba748f49c61961592a35bbb0c3fae
thisissernamalala <222650494@mycput.ac.za>
2024-08-16 21:16:03 +0200

    formatted integers exercise

--- part04-31_all_longest_in_list/src/all_longest_in_list.py
+++ part04-31_all_longest_in_list/src/all_longest_in_list.py
@@ -1,1 +1,13 @@
 # Write your solution here
+
+def all_the_longest(list):
+    long = []
+    result = []
+    for i in list:
+        long.append(len(i))
+    longest = max(long)
+
+    for j in list:
+        if len(j) == longest:
+            result.append(j)
+    return result
--- part04-32_integers_to_strings/src/integers_to_strings.py
+++ part04-32_integers_to_strings/src/integers_to_strings.py
@@ -1,1 +1,7 @@
 # Write your solution here
+
+def formatted(list):
+    newl = []
+    for i in list:
+        newl.append(f"{i:.2f}")
+    return newl
