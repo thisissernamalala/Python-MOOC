f5eb35f3c989af2e01f93b4170e8d031ae56b627
thisissernamalala <222650494@mycput.ac.za>
2024-08-16 21:38:08 +0200

    Reversed lists- a bit tricky

--- part04-33_everything_reversed/src/everything_reversed.py
+++ part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,12 @@
-# Write your solution here
+# Write your solution here\
+
+def everything_reversed(list):
+    newl = []
+    neww = []
+    for i in range(len(list)-1,-1,-1):
+        newl.append(list[i])
+        print(i)
+    print(newl)
+    for j in newl:
+        neww.append(j[::-1])
+    return neww
