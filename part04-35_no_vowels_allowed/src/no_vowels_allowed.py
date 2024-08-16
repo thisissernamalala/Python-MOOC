e7b8e7207ef5f4af76c68f7357ba5d03c259204f
thisissernamalala <222650494@mycput.ac.za>
2024-08-16 21:59:41 +0200

    no vowels exercise

--- part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,10 @@
 # Write your solution here
+
+def no_vowels(string):
+    neww = ""
+    for i in string:
+        if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
+            string.replace(i,"")
+        else:
+            neww+=i
+    return  neww
