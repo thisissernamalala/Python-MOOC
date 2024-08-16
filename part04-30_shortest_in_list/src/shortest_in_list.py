bb1569556588888c184356396543171cc9598227
thisissernamalala <222650494@mycput.ac.za>
2024-08-16 20:57:47 +0200

    shortest in list

--- part04-30_shortest_in_list/src/shortest_in_list.py
+++ part04-30_shortest_in_list/src/shortest_in_list.py
@@ -1,1 +1,12 @@
 # Write your solution here
+
+def shortest(list):
+    short = []
+    lenn = 0
+    for i in list:
+        short.append(len(i))
+    minn = min(short)
+    ind = short.index(minn)
+
+    return list[ind]
+
