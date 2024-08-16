642292b493b1a81778c5aa905808e1998b7409aa
thisissernamalala <222650494@mycput.ac.za>
2024-08-16 23:24:50 +0200

    Neighbours in list - quite difficult - have to redo

--- part04-37_neighbours_in_list/src/neighbours_in_list.py
+++ part04-37_neighbours_in_list/src/neighbours_in_list.py
@@ -1,1 +1,21 @@
 # Write your solution here
+
+def longest_series_of_neighbours(list):
+    current_count = 1
+    max_count = 0
+    longg = []
+
+    for i in range(len(list)):
+        if i < len(list) -1 and (list[i+1] == list[i]+1 or list[i+1] == list[i]-1) :
+            print(f"val: {list[i]}")
+            current_count += 1
+        elif i < len(list) -1 and (list[i+1] != list[i]+1 or list[i+1] != list[i]-1) :
+            print(f"val: {list[i]}")
+            if current_count>max_count:
+                max_count = current_count
+            current_count = 1
+            
+    if current_count>max_count:
+        max_count = current_count
+    
+    return max_count
