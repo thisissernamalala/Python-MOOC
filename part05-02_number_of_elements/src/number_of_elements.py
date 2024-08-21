8369a8bc9bd0b3618e8e42b260432e1e116fc635
thisissernamalala <222650494@mycput.ac.za>
2024-08-21 18:52:22 +0200

    Number of matching elements

--- part05-02_number_of_elements/src/number_of_elements.py
+++ part05-02_number_of_elements/src/number_of_elements.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def count_matching_elements(my_matrix: list, element: int):
+    count = 0
+    for i in my_matrix:
+        for j in i:
+            if j == element:
+                
+                count+=1
+    return count
+
+
